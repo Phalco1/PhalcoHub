37af0a95b766167772aa5b7c03fa80fdca6fef62
Phalco1 <elphalco1@gmail.com>
2023-10-02 16:14:32 -0500

    node 18

--- phalcohub-frontend-app/src/helpers/ProjectList.js
+++
@@ -1,9 +0,0 @@
-import Pacman from '../assets/Pacman.png';
-
-export const projectList = [
-    {
-        name: "Pacman Portfolio",
-        image: "Pacman.png",
-        skills: "ReactJS, Node.js, HTML, SCSS"
-    }
-];
