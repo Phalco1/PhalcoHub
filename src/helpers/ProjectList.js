eb226435af30be4b6601d320f18c2cfd15671931
Phalco1 <elphalco1@gmail.com>
2023-10-02 16:05:27 -0500

    new build

---
+++ src/helpers/ProjectList.js
@@ -0,0 +1,9 @@
+import Pacman from '../assets/Pacman.png';
+
+export const projectList = [
+    {
+        name: "Pacman Portfolio",
+        image: "Pacman.png",
+        skills: "ReactJS, Node.js, HTML, SCSS"
+    }
+];
